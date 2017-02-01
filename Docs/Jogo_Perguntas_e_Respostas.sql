60c3d9787aba285e52a564f89fb5b210ea758fb5
LucasMatheus123654789 <lucascasimirolcs@gmail.com>
2017-02-01 17:11:03 -0200

    atualização do banco de dados

--- Docs/Jogo_Perguntas_e_Respostas.sql
+++ Docs/Jogo_Perguntas_e_Respostas.sql
@@ -11,3 +11,33 @@ select * from tb_Jogador
 
 insert into tb_Jogador(nome) values('Amanda')
 insert into tb_Jogador(nome) values('Fernanda')
+
+create table tb_Perguntas(
+    id int identity primary key,
+    pergunta varchar(max),
+    resposta_correta varchar(max),
+    data    datetime default getdate()
+    )
+    
+    select * from tb_Perguntas	
+    
+    insert into tb_Jogador(pergunta, resposta_correta)
+    values('Qual é a cor do limâo?','verde')
+    
+    select * from tb_Jogador
+    
+    --deleta um id especifico na tabela--
+     
+    delete from tb_Jogador where id = 5
+    
+    --insere um novo id na tabela--
+    
+    insert into tb_Jogador(nome) values('Ramón')
+    
+    --deleta dados da tabela--
+    
+    delete from tb_Jogador
+   
+    --limpa a tabela e zera o campo identity--
+    
+    truncate table tb_jogador
